eb123e0741fa8c2573b1fceb6eb285e71d0dc8f0
Lao <estanislaolanus@gmail.com>
2022-06-14 21:12:07 -0300

    Primer commit

---
+++ tarea-1/script.js
@@ -0,0 +1,20 @@
+let numeroPrimo = parseFloat(prompt('Ingrese un numero'));
+
+let siEsPrimo = true;
+
+while(numeroPrimo < 0 || isNaN(numeroPrimo)){
+    numeroPrimo = parseFloat(prompt('Ingrese un numero'));
+}
+
+for (let i = 2; i <= numeroPrimo/2; i++) {
+    if (numeroPrimo % i === 0){
+        siEsPrimo = false;
+    }
+}
+
+if(siEsPrimo){
+    console.log('Es un numero primo');
+} else {
+    console.log('No es un numero primo')
+}
+
